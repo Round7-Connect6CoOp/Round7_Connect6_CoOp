5be20218c08c8779efc6b243505306f47d4f0750
ParkGyueun <22100271@handong.ac.kr>
2022-07-06 21:42:30 +0900

    Add gamelogic

--- app/src/main/java/com/gyeongoe/connect6ai/logic/GameLogic.java
+++ app/src/main/java/com/gyeongoe/connect6ai/logic/GameLogic.java
@@ -5,5 +5,24 @@ public class GameLogic {
 	// For Game Logic Blank null, Black 10, White 10, Effect range of black +1, Effect range of White -1, Ban 100
 	// Horizontal 0, Vertical 1, Left-Right Diagonal 2, Right-Left Diagonal 3
 	
-	
+	private int [][]info = new int[3][6];
+	private int point[][] = new int[3][6];
+	private int x,y;
+
+	for(int i=0; i<3; i++) {
+		for(int j=0; j<6; j++) {
+			if(i==0) {
+				info[i][j] = point[x-j][y-j];
+			}
+			else if(i==1) {
+				info[i][j] = point[x][y-j];
+			}
+			else if(i==2) {
+				info[i][j] = point[x+j][y-j];
+			}
+			else if(i==3) {
+				info[i][j] = point[x+j][y];
+			}
+		}
+	}
 }
